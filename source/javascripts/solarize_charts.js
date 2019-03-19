e7bfffbbd413aa942a1916c30f39e2a1044e6a73
Joy Chen <joyhzchen@gmail.com>
2019-03-18 22:28:50 -0400

    Add real data to chart and style caption

--- source/javascripts/solarize_charts.js
+++ source/javascripts/solarize_charts.js
@@ -6,14 +6,17 @@ var emissionsLineChart = new Chart(lineCtx, {
       datasets: [
         {
           label: 'Emissions 2008-2017',
-          data: [{x: 0, y: 1}, {x: 1, y: 2}],
+          data: [{x: 2008, y: 319743}, {x: 2010, y: 238420},
+            {x: 2012, y: 208616}, {x: 2014, y: 224650},
+            {x: 2016, y: 212100}, {x: 2017, y: 204200}
+           ],
           showLine: true,
           fill: false,
           borderColor: '#3F9B8F'
         },
         {
           label: 'Future emissions track',
-          data: [{x: 1, y: 2}, {x: 3, y: 4}],
+          data: [{x: 2017, y: 204200}, {x: 2019, y: 200000}],
           showLine: true,
           fill: false,
           borderColor: 'rgb(129, 195, 108)',
@@ -26,7 +29,7 @@ var emissionsLineChart = new Chart(lineCtx, {
       yAxes: [{
         scaleLabel: {
           display: true,
-          labelString: 'Emissions in'
+          labelString: 'Emissions in Metric Tons CO2 equivalent (MTCO2e)'
         },
       }],
       xAxes: [{
