5b1547711b8a6646cf75fd34d4fcd557e9473b4e
Lily  Pagan <lilypagan@Lilys-MBP.lan1>
2019-10-22 22:32:11 -0400

    slick carousel integration in progress

---
+++ source/javascripts/carousel.js
@@ -0,0 +1,10 @@
+//= require jquery
+//= require jquery.slick
+//= require rails-ujs
+
+$(document).ready(function(){
+  $('.past-projs-carousel').slick({
+		slidesToShow: 3,
+		slidesToScroll: 3
+  });
+});
